6dde418e3ffdcbb1e311d95f544d94866caf8127
mohammed bhuiyan <Mohammed.Bhuiyan6@va.gov>
2023-10-16 17:08:37 -0400

    First Commit in Coding Branch

---
+++ src/learnJava/UsingGitBranchExample.java
@@ -0,0 +1,14 @@
+package learnJava;
+
+public class UsingGitBranchExample {
+
+	public static void main(String[] args) {
+		        
+		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		for (String i : cars) {
+		  System.out.println(i);
+		}
+
+	}
+
+}
